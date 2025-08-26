eea7ec10453848d67c40d83434f33e97a550f6e3
John Behling <jkb1998@byu.edu>
2025-08-26 16:28:33 -0600

    Add Procfile

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--jbd%)ctmb38ao_-#noq5z_3+!zalxo-8-5n-37b-osvn2e76='
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--jbd%)ctmb38ao_-#noq5z_3+!zalxo-8-5n-37b-osvn2e76=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Assign DEBUG from environment variable 'DEBUG', defaulting to False
